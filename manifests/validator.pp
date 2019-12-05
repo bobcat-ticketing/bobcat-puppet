1a38a7d46f1a424aab6bd559bc32f8dd0377a747
Niklas Ederoth <niklas.ederoth@gmail.com>
2019-12-05 13:07:39 +0100

    added refresh api option to manifest

--- manifests/validator.pp
+++ manifests/validator.pp
@@ -6,6 +6,7 @@ class bobcat::validator (
   $dynconf_timer    = 'hourly',
   $python_version   = 'latest',
   $bobcat_version   = 'latest',
+  $refresh_api      = false
 ){
   require bobcat
   require bobcat::facts
